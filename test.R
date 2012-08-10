a665225331a64a4efeed241722002e4c6d2ad3e8
Max van der Heijden <max@shop2market.com>
2012-08-10 16:12:11 +0200

    Edits Max V2
    Hier nog wat edits

--- test.R
+++ test.R
@@ -1,8 +1,10 @@
 #Allow for a maximum of 5 affiliate entries per order
-#assists stays in the original form!
+#ABC stays in the original form!
 
-assists <- do.call(rbind, 
-        by(assists, INDICES=list(assists$Order.ID, assists$affiliate_id), 
+ABC <- do.call(rbind, 
+        by(ABC, INDICES=list(ABC$Order.ID, ABC$affiliate_id), 
            FUN=function(x) head(x[order(x$Unix.AssistTimestamp, decreasing=TRUE), ], 5))) 
 
-assists <- assists
+ABC <- ABC
+
+DAFUQ HAPPENED!??!!
